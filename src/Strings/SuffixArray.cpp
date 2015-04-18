616c427e3bcca6e334e2e661c6d10aa7fe282f70
Michael Harvey <wharvey@mymail.mines.edu>
2015-04-18 03:09:03 -0600

    small update

--- src/Strings/SuffixArray.cpp
+++ src/Strings/SuffixArray.cpp
@@ -12,25 +12,22 @@ namespace SuffixArray {
     int N, K, gap;
     vector<int> sa, rank, tmp, lcp;
 
-    bool cmp(int i, int j)
-    {
+    bool cmp(int i, int j) {
         if (rank[i] != rank[j]) return rank[i] < rank[j];
         i += gap; j += gap;
         return (i < N && j < N) ? rank[i] < rank[j] : i > j;
     }
 
-    void radixPass(int g)
-    {
+    void radixPass(int g) {
         vector<int> b(N), c(K+1);
-        for (int i=0; i<N; i++) (sa[i] + g < N) ? c[rank[sa[i]+g]]++ : c[0]++;
+        for (int i=0; i<N; i++) (sa[i]+g < N) ? c[rank[sa[i]+g]]++ : c[0]++;
         for (int i=0, sum=0; i<=K; i++) {int t = c[i]; c[i] = sum; sum += t; }
         for (int i=0; i<N; i++)
-            (sa[i] + g < N) ? b[c[rank[sa[i]+g]]++] = sa[i] : b[c[0]++] = sa[i];
+            (sa[i]+g < N) ? b[c[rank[sa[i]+g]]++] = sa[i] : b[c[0]++] = sa[i];
         sa = b;
     }
 
-    void buildSA()
-    {
+    void buildSA() {
         N = S.length();
         sa = rank = tmp = lcp = vector<int>(N);
         for (int i=0; i<N; i++) {
@@ -39,7 +36,7 @@ namespace SuffixArray {
         }
         for (gap = 1;; gap *= 2) {
             radixPass(gap); radixPass(0);
-            for (int i=0; i<N-1; i++) tmp[i + 1] = tmp[i] + cmp(sa[i], sa[i + 1]);
+            for (int i=0; i<N-1; i++) tmp[i+1] = tmp[i] + cmp(sa[i], sa[i+1]);
             for (int i=0; i<N; i++) rank[sa[i]] = tmp[i];
             K = tmp[N - 1];
             if (K == N) break;
@@ -47,8 +44,7 @@ namespace SuffixArray {
         for (int &r : rank) r--;
     }
 
-    void buildLCP()
-    {
+    void buildLCP() {
         for (int i=0, k=0; i<N; i++) if (rank[i] != N - 1) {
             for (int j = sa[rank[i] + 1]; S[i + k] == S[j + k];)
             ++k;
