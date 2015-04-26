ddc3bf13b8df050950965a6522fa59427b03a3d9
Michael Harvey <wharvey@mymail.mines.edu>
2015-04-25 22:59:43 -0600

    added ncr

---
+++ src/utilities/ncr.cpp
@@ -0,0 +1,28 @@
+/**
+ * Combinations (n choose k) using Pascal's triangle O(n^2)
+ */
+
+#include <iostream>
+using namespace std;
+
+const int MAXN = 1000, MOD = 1e9+7;
+long long comb[MAXN+1][MAXN+1];
+
+void build_table() {
+    for (int i=0; i<=MAXN; i++) {
+        for (int j=0; j<=i; j++) {
+            comb[i][j] = !j ? 1 : (comb[i-1][j-1] + comb[i-1][j]) % MOD;
+        }
+    }
+}
+
+/**
+ * Example usage
+ */
+int main() {
+    ios::sync_with_stdio(0);
+    build_table();
+    cout << "60 choose 9 mod 10^9+7 is " << comb[60][9] << "\n";
+    cout << "1000 choose 500 mod 10^9+7 is " << comb[1000][500] << "\n";
+    return 0;
+}
